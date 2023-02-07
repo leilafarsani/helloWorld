8dda4f356b8eb4d71f85156b0d0ad6b8a0204024
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-02-07 18:02:40 +0000

    Class Component!

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,4 +1,5 @@
 import ReactDom from "react-dom";
+import App from "./app";
 
 const reactEl = <h1>Hello World</h1>;
-ReactDom.render(reactEl, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
