8eadb2aecc2d4e87e4bacbe47d39dc3c9aa3e12b
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-02-07 18:24:11 +0000

    Functional Component

--- hello-world/src/app.js
+++ hello-world/src/app.js
@@ -1,9 +1,14 @@
 import { Component } from "react";
 
-class App extends Component {
-  render() {
-    return <h1>Hello World</h1>;
-  }
-}
+//class App extends Component {
+//render() {
+// return <h1>Hello World</h1>;
+// }
+//}
 
+
+
+function App() {
+  return <h1>Hello World</h1>;
+}
 export default App;
